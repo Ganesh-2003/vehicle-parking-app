793e6ea57af0e57fc71b37f2ed492b4df74ae73e
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-10-18 02:08:54 +0530

    Intial commit

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from models.users import create_user_table, register_user, check_user, fetch_user
 from controllers.admin_controller import dashboard
-from flask_restx import Namespace, Resource
 import bcrypt
 
 auth = Blueprint('auth',__name__)
